8fb5a714d2b36f25ced2dba0a49d3dc1920c2df5
v.dunkan <v.dunkan@gmail.com>
2022-01-12 01:53:31 +0200

    commit

--- module06/asyncio.py
+++ module06/asyncio.py
@@ -1,3 +1,4 @@
+'''https://goit.global/python-material-dev/docs/module-06/lesson-06-01#%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D1%8B-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B-%D1%81-asyncio'''
 import asyncio
 import aiohttp
 
