fcd8bf26a2bd3754bac55a083ea414bb144957a5
v.dunkan <v.dunkan@gmail.com>
2022-01-13 01:29:16 +0200

    commit

--- module12/code_snip_02.py
+++ module12/code_snip_02.py
@@ -1,3 +1,4 @@
+'''https://goit.global/python-material-dev/docs/module-12/lesson-12-01#%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D1%81%D0%BA%D0%B0%D1%8F-%D1%87%D0%B0%D1%81%D1%82%D1%8C'''
 import aiohttp
 import asyncio
 
